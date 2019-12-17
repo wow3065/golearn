012073acec81692a00593c4b3cef3b4d16357c02
liudengke <wow3065@126.com>
2019-12-17 15:51:42 +0800

    os env

--- first/main.go
+++ first/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("this is first go project")
+	fmt.Println("CPU数量：" + strconv.Itoa(runtime.NumCPU()))
+	fmt.Println("系统类型：" + runtime.GOOS)
+	fmt.Println("处理器架构：" + runtime.GOARCH)
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println("主机名：" + hostname)
+	}
+
 }
